fe21a0315cd17a05475d331ffe16b8d3bd3c4623
Raj <coolprince0186@gmail.com>
2026-03-26 23:08:55 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "https://task-manager-weld-alpha.vercel.app/"
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )

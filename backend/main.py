e1147fa7e355956172967a2548dee2525e7d0ce8
Raj <coolprince0186@gmail.com>
2026-03-26 23:23:39 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -8,11 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://task-manager-weld-alpha.vercel.app/"
-    ],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],

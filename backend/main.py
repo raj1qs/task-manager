540398e6b00a425c7ee132076c61b46abb0d9e16
princeqs <coolprince0186@gmail.com>
2026-03-26 10:34:51 +0530

    fixed CORS issue

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://task-manager-frontend-raj1qs.vercel.app", # Replace with your actual Vercel URL
+        "https://task-manager-phi-liart.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
